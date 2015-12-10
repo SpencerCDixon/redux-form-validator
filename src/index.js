3d8c248de2fdc78d7aeba853c779dbe2507a357a
Spencer Dixon <spencercdixon@gmail.com>
2015-12-10 16:11:23 -0500

    Fills out rest of package.json and properly does imports into index.js for distribution

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-function KeyError(msg) {
-  this.message = msg;
-  this.name = "KeyError";
-}
+import { formValidator } from 'formValidator';
 
-export function formValidator(fields) {
-  var errors = {};
-  var fieldKeys = Object.keys(fields);
-
-  return function(values) {
-
-    for (var key in values) {
-      console.log(key);
-      if (key in fields) {
-        console.log('Key is there:', key)
-      } else {
-        var msg = key + " key wasn't found";
-        throw new KeyError(msg)
-      }
-    }
-
-  }
+export {
+  formValidator
 }
